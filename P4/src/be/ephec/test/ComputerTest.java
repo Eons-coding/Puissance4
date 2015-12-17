b0bad2014485b643cad2c4e8d42b679330e6d07b
Xavier <dechampsx@gmail.com>
2015-12-17 23:29:00 +0100

    JUnit 1st try

---
+++ P4/src/be/ephec/test/ComputerTest.java
@@ -0,0 +1,17 @@
+package be.ephec.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComputerTest {
+
+	@Test
+	public void testNbAlignVertic() {
+		
+		
+		
+		fail("Not yet implemented");
+	}
+
+}
